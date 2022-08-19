eb3c5b4ee6db95d385c0cab61c2cb983e836b759
Mascapecha <juan.francisco.9601@gmail.com>
2022-08-19 12:50:13 -0300

    edición de los modelos

--- Informatorio2022/blog/models.py
+++ Informatorio2022/blog/models.py
@@ -5,10 +5,26 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Post(models.Model):
+class Evento(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
-    texto = models.TextField()
+    nombre_evento = models.CharField(max_length=200)
+    fecha_hora_evento = models.DateField(auto_now=False, auto_now_add=False)
+    informacion = models.CharField(max_length=200)
+
+    def publicar(self):
+        self.fecha_publcacion = timezone.now()
+        self.save()
+
+    def eliminar(self):
+        self.fecha_eliminacion = timezone.now()
+        self.delete()                                      #lo elimina de la base de datos también?
+
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    id_evento = models.ForeignKey(Evento, on_delete=models.CASCADE)   #importa el orden en el que se crean los modelos? está bien definida esta FK?
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()  
     fecha_creacion = models.DateTimeField(default=timezone.now)
     fecha_publcacion = models.DateTimeField(blank=True, null=True)
 
@@ -20,16 +36,13 @@ class Post(models.Model):
         return self.titulo
 
 class Imagen(models.Model):
+    id_post = models.ForeignKey(Post, on_delete=models.CASCADE)
     descripcion = models.CharField(max_length = 200)
 
 class Video(models.Model):
+    id_post = models.ForeignKey(Post, on_delete=models.CASCADE)
     descripcion = models.CharField(max_length=200)
 
-class evento(models.Model):
-    titulo = models.CharField(max_length=200)
-    nombre_evento = models.CharField(max_length=200)
-    fecha_hora_evento = models.DateField(auto_now=False, auto_now_add=False)
-    fecha_hora_pub_evento = models.DateField(auto_now=False, auto_now_add=True)
-    fecha_hora_elim_evento = models.DateField(auto_now=False, auto_now_add=True)
-    informacion = models.CharField(max_length=200)
-        
+
+    
+    
