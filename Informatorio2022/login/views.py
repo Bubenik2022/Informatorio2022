0b809a60a6161c95beaf65c4243244f3fae90605
Mascapecha <juan.francisco.9601@gmail.com>
2022-08-26 12:05:13 -0300

    redirección para loguearse luego de crearse el usuario

--- Informatorio2022/login/views.py
+++ Informatorio2022/login/views.py
@@ -7,4 +7,4 @@ from django.urls import reverse_lazy
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
-    success_url = reverse_lazy('registro')
+    success_url = reverse_lazy('login')
